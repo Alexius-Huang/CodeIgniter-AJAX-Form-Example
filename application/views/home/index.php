d4c55251f7f4a5c3eba596206eb768da7979bedd
Maxwell-Alexius <destinydragon0614@yahoo.com.tw>
2016-07-15 14:51:54 +0800

    Basic style initialized

---
+++ application/views/home/index.php
@@ -0,0 +1,37 @@
+<!-- Buttons -->
+<div id="home-button">
+	<a href="#" id="show-post-link">Browse Posts</a>
+	<a href="#" id="new-post-form-link">New Post</a>
+</div>
+
+<div id="show-post">
+	<!-- Post Table -->
+	<table>
+		<thead>
+			<tr>
+				<th>Title</th>
+				<th>Content</th>
+				<th>Create Date</th>
+				<th>Actions</th>
+			</tr>
+		</thead>
+		<tbody>
+			<!-- Posts Information -->
+		</tbody>
+	</table>
+</div>
+
+<div id="new-post">
+	<!-- New Post Form -->
+	<form action="" id="new-post-form" method="POST">
+		
+		<label for="title">Title</label><br>
+		<input type="text" name='title' class="text" /><br><br>
+
+		<label for="content">Content</label><br>
+		<textarea name="content" cols="30" rows="10"></textarea><br><br>
+
+		<input type="submit" name="submit" class="submit" />
+
+	</form>
+</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -6,6 +6,7 @@
 	<script src="https://code.jquery.com/jquery-1.10.2.min.js" integrity="sha256-C6CB9UYIS9UJeqinPHWTHVqh/E1uhG5Twh+Y5qFQmYg=" crossorigin="anonymous"></script>
 	<script src="https://code.jquery.com/ui/1.10.3/jquery-ui.min.js" integrity="sha256-lnH4vnCtlKU2LmD0ZW1dU7ohTTKrcKP50WA9fa350cE=" crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="<?php echo asset_url(); ?>css/main.css">
+	<link href='https://fonts.googleapis.com/css?family=Ubuntu:300' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<h2><?php echo $title; ?></h2>	
+	<br><h2><?php echo $title; ?></h2><br>
